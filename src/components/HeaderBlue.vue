8c7084d22c19db40e8be3785b208abcebb3ff849
zeynab <zeynab.sharifi.1378@gmail.com>
2021-12-28 02:44:10 -0800

    create login component install sass and axios

--- src/components/HeaderBlue.vue
+++ src/components/HeaderBlue.vue
@@ -1,41 +1,29 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#">Blue</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <router-link class="nav-link" to="/">Home</router-link>
-        <router-link class="nav-link" to="/Game">Game</router-link>
-        <router-link class="nav-link" to="/Game">Game</router-link>
-        <router-link class="nav-link" to="/Support">Support</router-link>
-      </div>
+<header>
+    <div class="header-blue">
+            <nav class="navbar navbar-expand-lg">
+              <div class="container">
+                <a class="navbar-brand" href="#">Blue</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav">
+                    <router-link class="nav-link" to="/">Home</router-link>
+                    <router-link class="nav-link" to="/Game">Game</router-link>
+                    <router-link class="nav-link" to="/About">About</router-link>
+                    <router-link class="nav-link" to="/Support">Support</router-link>
+                  </div>
+                </div>
+                <!-- login form in header -->
+               <login></login>
+          </div>
+      </nav>
     </div>
-    <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-  </div>
-</nav>
+</header>
 </template>
+<script>
+export default {
+
+}
+</script>
