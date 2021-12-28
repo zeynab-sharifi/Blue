5ec2631ea619d78b2b261bc835a22d95c2306839
zeynab <zeynab.sharifi.1378@gmail.com>
2021-12-27 18:49:12 -0800

    create footer component and add html code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <header-blue></header-blue>
+  
   <router-view></router-view>
+  <footer-blue></footer-blue>
 </template>
 
 <script>
-import HeaderBlue from "./components/HeaderBlue.vue"
+import HeaderBlue from "./components/HeaderBlue.vue";
+import FooterBlue from "./components/FooterBlue.vue"
 export default {
   name: 'App',
   components:{
-    HeaderBlue
+    HeaderBlue,
+    FooterBlue
   }
 }
 </script>
---
+++ src/components/FooterBlue.vue
@@ -0,0 +1,57 @@
+<template>
+    <footer>
+        <div class="footer-blue">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <h4>Action</h4>
+                        <ul>
+                            <li><a href="">pubg mobaile</a></li>
+                            <li><a href=""> call of duty</a></li>
+                            <li><a href="">free fire</a></li>
+                            <li><a href="">arena of fire</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <h4>Action</h4>
+                        <ul>
+                            <li><a href="">pubg mobaile</a></li>
+                            <li><a href=""> call of duty</a></li>
+                            <li><a href="">free fire</a></li>
+                            <li><a href="">arena of fire</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <h4>Action</h4>
+                        <ul>
+                            <li><a href="">pubg mobaile</a></li>
+                            <li><a href=""> call of duty</a></li>
+                            <li><a href="">free fire</a></li>
+                            <li><a href="">arena of fire</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <h4>Action</h4>
+                        <ul>
+                            <li><a href="">pubg mobaile</a></li>
+                            <li><a href=""> call of duty</a></li>
+                            <li><a href="">free fire</a></li>
+                            <li><a href="">arena of fire</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="sotial-media-footer">
+                            <i class="fab fa-instagram"></i>
+                            <i class="fab fa-telegram"></i>
+                            <i class="fab fa-whatsapp"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
