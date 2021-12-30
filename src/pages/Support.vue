31ed977e373ef3c0975019e6b392e6f2491a3131
zeynab <zeynab.sharifi.1378@gmail.com>
2021-12-30 01:12:03 -0800

    add html and scss with support

--- src/pages/Support.vue
+++ src/pages/Support.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>support</h1>
+    <div class="support-form">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
