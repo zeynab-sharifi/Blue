dee25ebba8aab971cb3ee76cd42de0030bcb4345
zeynab <zeynab.sharifi.1378@gmail.com>
2022-01-15 09:58:02 -0800

    add commentList page data

--- src/pages/Layer/comment/CommentList.vue
+++ src/pages/Layer/comment/CommentList.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="container">
         <div class="row">
-           <div class="col-lg-6 col-md-6 col-sm-12"></div>
+           <div class="col-lg-6 col-md-6 col-sm-12" v-for="Comment in Comments" :key="Comment.id">
+               <div class="box-comment-list">
+                    <h3>{{ Comment.name }}</h3>
+                    <p>{{ Comment.body }}</p>
+               </div>
+           </div>
         </div>
     </div>                                  
 </template>
@@ -12,13 +17,13 @@ import { ref } from "vue";
 import { useRoute } from "vue-router";
     export default{
         setup(){
-            const comments = ref([])
+            const Comments = ref([])
             const route = useRoute();
 
             function getComment(){
                 axios.get('https://jsonplaceholder.typicode.com/comments')
                 .then(function (response) {
-                    comments.value = response.data;
+                    Comments.value = response.data;
                 })
                 .catch(function (error) {
                     console.log(error);
@@ -27,7 +32,7 @@ import { useRoute } from "vue-router";
             getComment();
 
             return{
-                comments,
+                Comments,
                 route
             }
         }
